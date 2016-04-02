095ef4c3092e6a49d69306b31b2081b6b09e9fd3
Foeh Mannay <foeh.mannay@gmail.com>
2016-04-02 23:39:32 +0100

    Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # ospf2dot
-Converts the output of Cisco IOS "show ip ospf database neighbor" into a GraphViz DOT file
+
+Converts the output of Cisco IOS command "show ip ospf database neighbor" into
+a GraphViz DOT file.
+
+GraphViz can then plot the DOT file into a vertex / edge style graph depicting
+your network topology and the associated OSPF costs. Any asymmetric link costs
+(different cost set on each end of the link) show up in red.
+
+This script requires Python 3
+
+Usage - Linux / Mac
+===================
+
+Set the ospf2dot.py file as executable (chmod 755 ospf2dot.py) then run it
+
+Usage - Windows
+===============
+
+Run python 3 then execute:
+
+exec(open("./ospf2dot.py").read())
+
+Example
+=======
+
+foeh@feeble ~ $ ./ospf2dot.py
+ospf2dot - takes the output of "show ip ospf database router" and outputs a
+GraphViz DOT file corresponding to the network topology
+
+v0.1 alpha, By Foeh Mannay, April 2016
+
+Enter input filename: cli-output.txt
+Enter output filename: mytopology.dot
+foeh@feeble ~ $
+
+Using DOT files
+===============
+
+Several applications can deal with DOT files.
+
+Example: Native graphviz "dot" processor to output a .gif file:
+
+foeh@feeble ~ $ dot -Tgif -omytopology.gif mytopology.dot
+
+(Or use xdot under Linux / gvedit under Windows to see it directly)
+
+Bugs
+====
+
+So far I have only tested this againts one device's output. I expect that means
+it will break when output from other devices is used :D
+
+Oh, and I won't lie to you, I write terrible Python.
---
+++ ospf2dot.py
@@ -0,0 +1,189 @@
+#!/usr/bin/python3
+
+# ospf2dot - by Foeh Mannay
+#
+# Converts the output of the "show ip ospf database router" command into a GraphViz DOT file.
+#
+# The DOT file can then be used to automatically plot network topology diagrams and, while the layout
+# can often leave a lot to be desired, it is very easy to spot asymmetric OSPF costs and other anomalies.
+
+import re
+
+def toslash(str):
+	# Dictionary to convert masks to slash notation
+	return {
+		'0.0.0.0':			'/0',
+		'128.0.0.0':		'/1',
+		'192.0.0.0':		'/2',
+		'224.0.0.0':		'/3',
+		'240.0.0.0':		'/4',
+		'248.0.0.0':		'/5',
+		'252.0.0.0':		'/6',
+		'254.0.0.0':		'/7',
+		'255.0.0.0':		'/8',
+		'255.128.0.0':		'/9',
+		'255.192.0.0':		'/10',
+		'255.224.0.0':		'/11',
+		'255.240.0.0':		'/12',
+		'255.248.0.0':		'/13',
+		'255.252.0.0':		'/14',
+		'255.254.0.0':		'/15',
+		'255.255.0.0':		'/16',
+		'255.255.128.0':	'/17',
+		'255.255.192.0':	'/18',
+		'255.255.224.0':	'/19',
+		'255.255.240.0':	'/20',
+		'255.255.248.0':	'/21',
+		'255.255.252.0':	'/22',
+		'255.255.254.0':	'/23',
+		'255.255.255.0':	'/24',
+		'255.255.255.128':	'/25',
+		'255.255.255.192':	'/26',
+		'255.255.255.224':	'/27',
+		'255.255.255.240':	'/28',
+		'255.255.255.248':	'/29',
+		'255.255.255.252':	'/30',
+		'255.255.255.254':	'/31',
+		'255.255.255.255':	'/32',
+	}[str]
+
+def toDecimal(addr):
+	# Converts an IP address to a decimal
+	addrlist = addr.split('.')
+	return(int(addrlist[3]) + 256 * (int(addrlist[2]) + 256 * (int(addrlist[1]) + 256 * int(addrlist[0]))))
+	
+def sameP2P(addr1, addr2):
+	# Returns true if the two addresses provided are one apart (assume same /30 or /31 network)
+	if(abs(toDecimal(addr1) - toDecimal(addr2)) == 1):
+		return True
+	else:
+		return False
+	
+def Reduce(list):
+	# Takes a list of links and merges entries for two ends of the same link, provided the metric matches
+	l = 0
+	while(l < len(list) - 1):
+		if(list[l][0] == list[l+1][1] and list[l+1][0] == list[l][1] and sameP2P(list[l][2], list[l+1][2]) and list[l][3] == list[l+1][3]):
+			# if two links are A->B and B->A and in same subnet and have same metric then merge into an undirected edge
+			list[l][4]='none'
+			list.remove(list[l+1])
+		l = l + 1
+	return(list)
+
+def mergeSort(list):
+	# Performs a standard merge sort on a list of link entries based on IP address
+	sorted = []
+	listlen = len(list)
+	i = 0
+	j = 0
+	
+	if(listlen > 1):
+		# If we have 2 or more items, split the list and merge sort each half
+		left=mergeSort(list[:listlen//2])
+		right=mergeSort(list[listlen//2:])
+		# Then merge the two sorted halves together
+		while(i < len(left) and j < len(right)):
+			if(toDecimal(left[i][2]) < toDecimal(right[j][2])):
+				sorted.append(left[i])
+				i = i + 1
+			else:
+				sorted.append(right[j])
+				j = j + 1
+		while(i < len(left)):
+			sorted.append(left[i])
+			i = i + 1
+		while(j < len(right)):
+			sorted.append(right[j])
+			j = j + 1
+	else:
+		sorted = list
+	return(sorted)
+
+class Router:
+	# Class to store a single router's identity, stub networks and links
+	def __init__(self, rid):
+		self.routerid = rid
+		self.hostname = rid
+		self.stubs = []
+		self.links = []
+	
+	def sethostname(self, str):
+		self.hostname = str
+	
+	def addstub(self, subnet, mask, metric):
+		self.stubs.append([subnet, mask, metric])
+	
+	def addlink(self, neighbour, ip, metric):
+		self.links.append([neighbour, ip, metric])
+	
+	def dottifyrouter(self):
+		# Produces a DOT representation of the router this object represents
+		rv = ('\th' + re.sub('\.','x',self.routerid) + ' [label="' + self.routerid + '\\n' + self.hostname)
+		# Unhash this if you want all stubs to be listed on your nodes
+		#for i in self.stubs:
+		#	rv += ('\n' + i[0] + toslash(i[1]))
+		rv +=('"]\n')
+		return(rv)
+
+routers=[]
+links=[]
+
+print("ospf2dot - takes the output of \"show ip ospf database router\" and outputs a GraphViz DOT file corresponding to the network topology\n")
+print("v0.1 alpha, By Foeh Mannay, April 2016\n")
+
+filename = input("Enter input filename: ")
+
+with open(filename, 'r') as infile:
+	for line in infile:
+		m = re.search('Link State ID: (\d*.\d*.\d*.\d*)', line)
+		if(m):
+			rtr=Router(m.group(1))
+			routers.append(rtr)
+			continue
+		m = re.search('Advertising Router: (\S*)', line)
+		if(m):
+			rtr.sethostname(m.group(1))
+			continue
+		m = re.search('\(Link ID\) Network/subnet number: (\d*.\d*.\d*.\d*)', line)
+		if(m):
+			stubnet = m.group(1)
+			continue
+		m = re.search('\(Link Data\) Network Mask: (\d*.\d*.\d*.\d*)', line)
+		if(m):
+			stubmask = m.group(1)
+			continue
+		m = re.search('\(Link ID\) Neighboring Router ID: (\d*.\d*.\d*.\d*)', line)
+		if(m):
+			neighbour = m.group(1)
+			continue
+		m = re.search('\(Link Data\) Router Interface address: (\d*.\d*.\d*.\d*)', line)
+		if(m):
+			interfaceip = m.group(1)
+			continue
+		m = re.search('TOS 0 Metrics: (\d*)', line)
+		if(m):
+			if(stubnet is None):
+				rtr.addlink(neighbour, interfaceip, m.group(1))
+				neighbour=None
+				interfaceip=None
+			else:
+				rtr.addstub(stubnet, stubmask, m.group(1))
+				stubnet=None
+				stubmask=None
+			continue
+
+filename = input("Enter output filename: ")
+with open(filename, 'w') as outfile:
+	outfile.write("digraph Topology {\n")
+	for r in routers:
+		# Ask each Router object in turn to describe itself
+		outfile.write(r.dottifyrouter())
+	for r in routers:
+		for l in r.links:
+			# Create a list of all router links (src, dest, IP, metric, style)
+			links.append([r.routerid, l[0], l[1], l[2], 'forward color="red"'])
+	for l in Reduce(mergeSort(links)):
+		# Pair up symmetrically costed links (so we get an undirected edge rather than two directed edges) then output to the DOT file 
+		outfile.write('\th' + re.sub('\.','x',l[0]) + ' -> h' + re.sub('\.','x',l[1]) + '[label="' + l[3] + '", dir=' + l[4] + ']\n')
+	outfile.write("}\n")
+
